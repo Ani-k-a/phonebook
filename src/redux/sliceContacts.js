69a4b425c5b13ea13f988773822806e927e0f202
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-06-19 22:34:17 +0200

    add case

--- src/redux/sliceContacts.js
+++ src/redux/sliceContacts.js
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchContacts } from "./operations";
+import { fetchContacts, deleteContact, addContact } from "./operations";
+
+const handlePending = state => state.isLoading = true;
+const handleRejected = (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
+};
+
 
 export const contactsSlice = createSlice({
     name: 'contacts',
@@ -8,19 +15,36 @@ export const contactsSlice = createSlice({
         isLoading: false,
         error: null
     },
-    extraReducers: {
-        [fetchContacts.pending](state) {
-            state.isLoading = true;
-        },
-        [fetchContacts.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = action.payload;
-        },
-        [fetchContacts.rejected](state, action) {
-            state.isLoading = false;
-            state.error = action.payload;
-        }
+    extraReducers: builder => {
+        builder
+            .addCase(fetchContacts.pending, handlePending)
+            .addCase(addContact.pending, handlePending)
+            .addCase(deleteContact.pending, handlePending)
+
+            .addCase(fetchContacts.rejected, handleRejected)
+            .addCase(addContact.rejected, handleRejected)
+            .addCase(deleteContact.rejected, handleRejected)
+
+            .addCase(fetchContacts.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = action.payload;
+
+            })
+            .addCase(addContact.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items.push(action.payload);
+
+            })
+            .addCase(deleteContact.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                const index = state.items.findIndex(item => item.id === action.payload.id);
+                state.items.splice(index, 1);
+
+            })
+
     }
 }
 );
